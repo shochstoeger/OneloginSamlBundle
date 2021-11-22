4c1ec2c5c66675424209c6dc4802b2a298cb8fcd
shochstoeger <78364221+shochstoeger@users.noreply.github.com>
2021-11-22 13:04:06 +0100

    Update SecurityCompilerPass.php
    updated config to allow environment variables

--- DependencyInjection/Compiler/SecurityCompilerPass.php
+++ DependencyInjection/Compiler/SecurityCompilerPass.php
@@ -14,6 +14,7 @@ class SecurityCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         $configs = $container->getExtensionConfig('hslavich_onelogin_saml');
+        $configs = $container->resolveEnvPlaceholders($configs, true);
         $config = $this->processConfiguration(new Configuration(), $configs);
 
         if (!empty($config['entityManagerName'])) {
